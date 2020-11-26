4810022b23199853362b928fee95cfed56c0791b
HMB <1352482583@qq.com>
2020-11-27 01:57:06 +0800

    改爬虫后的

--- src/Main.scala
+++ src/Main.scala
@@ -1,13 +1,15 @@
 import scala.io.{BufferedSource, Source}
+import scala.collection.mutable
 /**
   * @author hmb
   */
 object Main {
 
-  val headsList: Seq[String] = "timestamp,volume,open,high,low,close,chg,percent,turnoverrate,amount,volume_post,amount_post,pe,pb,ps,pcf,market_capital,balance,hold_volume_cn,hold_ratio_cn,net_volume_cn,hold_volume_hk,hold_ratio_hk,net_volume_hk".split(",").toList
+  val headsList: Seq[String] = "timestamp,volume,open,high,low,close,chg,percent,turnoverrate,amount,volume_post,amount_post,pe,pb,ps,pcf,market_capital,balance,hold_volume_cn,hold_ratio_cn,net_volume_cn,hold_volume_hk,hold_ratio_hk,net_volume_hk,stock_code".split(",").toList
   val headToIndex: Map[String, Int] = Array.range(0, headsList.length).map(i => (headsList(i), i)).toMap
 
   val TIME_STAMP = "timestamp"
+  val STOCK_CODE = "stock_code"
 
   val HIGH = "high"//最高价
   val TURN_OVER_RATE = "turnoverrate"//周转率
@@ -19,9 +21,17 @@ object Main {
   private val mostImportantMerits: Seq[String] = Array(HIGH, TURN_OVER_RATE, PERCENT, VOLUME, PE, PB).toList
   private val mostImportantIndexes: Seq[Int] = mostImportantMerits.map(s=>headToIndex(s))
 
-  def myread(path:String) :Seq[String] = {
+  private var sharesMap:mutable.Map[String, Shares] = mutable.Map()
+  private var moduleMap:mutable.Map[String, Shares] = mutable.Map()
+
+
+  def myRead(path:String) :Seq[String] = {
     val src: BufferedSource = Source.fromFile(path, "UTF-8")
     src.getLines().toList
+  }
+
+  def myGet(seq: Seq[String], string: String): String ={
+    seq(headToIndex(string))
   }
 
   def main(hmb666:Array[String]): Unit ={
@@ -33,15 +43,22 @@ object Main {
 //    headToIndex.foreach(println)
 //    mostImportantIndexes.foreach(println)
 
-    var lines: Seq[Seq[String]] = myread("SZ300008-1606154245000.csv")
-  .drop(1)
-  .map(line=>line.split(",", -1).map(s=> if (s.isEmpty) "0" else s).toList)
+    var lines: Seq[Seq[String]] = myRead("SZ300008-1606154245000.csv")
+      .map(line=>line.split(",", -1).map(s=> if (s.isEmpty) "0" else s).toList)
+
+    for (line: Seq[String] <- lines){
+      var stock_code = myGet(line, STOCK_CODE)
+      if(!sharesMap.contains(stock_code)){
+        sharesMap += (stock_code->new Shares())
+      }
+      sharesMap(stock_code).push(line)
+    }
 
 //    var newlines: Seq[Seq[String]] = lines.map(line =>mostImportantIndexes.map(ptr=>line(ptr) ) )
 //    newlines.foreach(s=>println(s.mkString(",")))
 
 
-    lines.foreach(println)
+    //lines.foreach(println)
 
 
   }
--- src/Shares.scala
+++ src/Shares.scala
@@ -1,41 +1,39 @@
-import Main.TIME_STAMP
-//import scala.collection.mutable.{Seq,Map}
+import Main.{TIME_STAMP, myGet}
+
 
 /**
   * @author hmb 股票
   */
 class Shares {
 
-  val MAX_INT: Int = 30
+  val MAX_SIZE: Int = 30
 
-  var sharesMessages : Seq[Map[String, String]] = Seq()//每一个map：股票的属性->值，所有的属性，很多天所以就是列表了
-  private var allTimes: Seq[Int] =sharesMessages.map(each_map=>each_map(TIME_STAMP)).map(_.toInt)//用set可以优化
+  var sharesMessages : Seq[Seq[String]] = Seq()//每一个map：股票的属性->值，所有的属性，很多天所以就是列表了
+  private var allTimes: Seq[Long] =sharesMessages.map(each_map=>myGet(each_map, TIME_STAMP)).map(_.toLong)//用set可以优化
 
-  def this(seq: Seq[String]) ={
-    this()
-  }
 
-  def push(my_map:Map[String, String]): Unit = {
-    val currentTime: Int = my_map(TIME_STAMP).toInt
+  def push(my_seq:Seq[String]): Unit = {
+    val currentTime: Long = myGet(my_seq, TIME_STAMP).toLong
 
-    if (allTimes.contains(currentTime)){
-      return
-    }
+//    if (allTimes.contains(currentTime)){    //看了看爬虫，可能不再需要太多的判断了。关于times的或许能删除了。
+//      return
+//    }
 
-    if(sharesMessages.length >= MAX_INT){
-      val minimalTime:Int = allTimes.min//可以优化，若allTimes为空，可能报error
+    val minimalTime:Long = if(allTimes.isEmpty) 0 else allTimes.min//可以优化
+
+    if(sharesMessages.length >= MAX_SIZE){
       if(currentTime < minimalTime){
         //我们的数据更旧，不操作
         return
       }
       else{
         //挑选一个最小的，删了那个最小的
-        sharesMessages = sharesMessages.filter(each_map=>each_map(TIME_STAMP).toInt > minimalTime)//不可变的
+        sharesMessages = sharesMessages.filter(each_map=>myGet(each_map, TIME_STAMP).toLong > minimalTime)//不可变的
       }
     }
 
-    sharesMessages :+ my_map //不可变的，可能会报错
-    allTimes = sharesMessages.map(each_map=>each_map(TIME_STAMP)).map(_.toInt)//优化了，就必须要同步更新了
+    sharesMessages = sharesMessages :+ my_seq //不可变的，可能会报错
+    allTimes = sharesMessages.map(each_map=>myGet(each_map, TIME_STAMP)).map(_.toLong)//优化了，就必须要同步更新了
   }
 
 
